5320c6c333b37ea25ae2d7692eec73a088e7671b
Bruno Marcenaro <bems2588@gmail.com>
2022-03-14 19:15:50 -0400

    add values

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const { endianness } = require('os');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,26 @@ const app = express();
 //  EXPRESS MIDDLEWARE
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+
+
+//CONNECT TO DATABASE
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    //Your MySQL username,
+    user: 'root',
+    //Your MySQL password
+    password: 'Password',
+    database: 'election'
+  },
+  console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 //DEFAULT RESPONSE FOR ANY OTHER REQUEST(NOT FOUND)
 
