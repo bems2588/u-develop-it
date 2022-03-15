28acec03a8b65ca51a97926b7518b9d015df556d
Bruno Marcenaro <bems2588@gmail.com>
2022-03-14 20:04:15 -0400

    add candidates

--- server.js
+++ server.js
@@ -24,8 +24,37 @@ const db = mysql.createConnection(
   console.log('Connected to the election database')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+//GET A SINGLE CANDIDATE
+// db.query(`SELECT * FROM candidates WHERE id = 1`,(err, row) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+//DELETE A CANDIDATE
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+//CREATE A CANDIDATE
+
+const sql = ` INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES(?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 //DEFAULT RESPONSE FOR ANY OTHER REQUEST(NOT FOUND)
